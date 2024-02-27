e1000e05f63229a2f2235f26acd8b6e905af3b44
ItzMeRainy <rayyanyasirk@gmail.com>
2024-02-27 16:33:46 +0500

    Fixed comment errors.

--- Data Structures/Queue.py
+++ Data Structures/Queue.py
@@ -4,7 +4,7 @@ TailPointer = 0 # Elements added are sent to the back or the tail of the queue.
 HeadPointer = 0 # Elements removed are dequeued from the beginning or head of the queue.
 Entries = 0 # Represents total number of elements currently present in queue.
 
-# Method to add number to queue.
+# Procedure to add number to queue.
 def Enqueue():
     global TailPointer, Entries
     num = int(input("Enter number to add: "))
@@ -17,7 +17,7 @@ def Enqueue():
     else:
         print("Queue is Full")
 
-# Method to remove number from queue.
+# Procedure to remove number from queue.
 def Dequeue():
     global HeadPointer, Entries
     if HeadPointer >= QueueLength - 1:
--- Data Structures/Stack.py
+++ Data Structures/Stack.py
@@ -1,7 +1,7 @@
 Stack = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # 10 element stack, defined and initialized.
 StackPointer = 0 # Pointer set to start of stack. Increments for every element added.
 
-# Method to add number to stack.
+# Procedure to add number to stack.
 def AddToStack():
     global StackPointer # Make sure stackpointer is set to global inside function/method.
     num = int(input("Enter a number to add: "))
@@ -11,7 +11,7 @@ def AddToStack():
     else:
         print("Stack is Full")
 
-# Method to remove number from stack.
+# Procedure to remove number from stack.
 def RemoveFromStack():
     global StackPointer # Make sure stackpointer is set to global inside function/method.
     if StackPointer >= 0:
