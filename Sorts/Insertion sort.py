6c3db0ac7b30b101dba0923d4e0ce88e3deda62b
cheesesocrates <160471222+cheesesocrates@users.noreply.github.com>
2024-03-15 17:14:54 +0500

    Update Insertion sort.py

--- Sorts/Insertion sort.py
+++ Sorts/Insertion sort.py
@@ -1,7 +1,7 @@
 def insertsort(list):
-    for x in range(len(list)):
-        sorting = x
-        while sorting > 0 and list[sorting] < list[sorting - 1]:
+    for x in range(len(list)):                                                                      #main iteration loop
+        sorting = x 
+        while sorting > 0 and list[sorting] < list[sorting - 1]:                                    #if it reaches 0 or is greater than prev no. it will end while
             list[sorting], list[sorting - 1] = list[sorting - 1], list[sorting]
             sorting -= 1
 
