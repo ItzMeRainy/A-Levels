65a908ffcf97959a0dcf3310cacde20ec40b0571
ItzMeRainy <rayyanyasirk@gmail.com>
2024-03-24 19:40:44 +0500

    fixed linear search

--- Searches/Linear search.py
+++ Searches/Linear search.py
@@ -1,18 +1,14 @@
-def linsearch(array, number):
+def LinearSearch(List, Number):
 
-    count = 0
-    search = number
-
-    for i in range(1, len(array)):
-       if search == array[i]:
-            count = count + 1
-            print("searched item found at index ", i + 1, ",", count, "time/times")
-
-    if count == 0:
-       print("item not found in list")
-
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-linsearch(array, 5)
+   for i in range(len(List)):
+      if Number == List[i]:
+         print(f"'{Number}' found at index: {i}")
+         return
+      
+   print("Number not found.")
+   
+MyList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+LinearSearch(MyList, 20)
 
 # A linear search is the simplest approach employed to search for an element in a data set.
 # It examines each element until it finds a match, starting at the beginning of the data
